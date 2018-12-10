b3c665a69b0c37337cab4471da979e44cafd0fd2
mochiThanakorn <tnksp9@gmail.com>
2018-12-10 15:06:17 +0700

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,7 @@
-const functions = require('firebase-functions');
-const firebase = require('firebase-admin');
-const express = require('express');
-const engines = require('consolidate');
-const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
-const cors = require('cors');
-const tokenApi = "CC80274793D170ADF2FE745398A9C458D81357557F89392203C0052CE4F99748";
+const functions = require('firebase-functions')
+const express = require('express')
+const cors = require('cors')
+const tokenApi = "CC80274793D170ADF2FE745398A9C458D81357557F89392203C0052CE4F99748"
 
 const fabricRoll = require('./api_modules/fabricRoll')
 const fabricType = require('./api_modules/fabricType')
@@ -16,8 +12,8 @@ const supplier = require('./api_modules/supplier')
 const customer = require('./api_modules/customer')
 const signin = require('./api_modules/signin')
 
-const api = express();
-api.use(cors());
+const api = express()
+api.use(cors())
 api.use('/fabricroll',fabricRoll)
 api.use('/fabrictype',fabricType)
 api.use('/fabriccolor',fabricColor)
@@ -28,8 +24,8 @@ api.use('/user',user)
 api.use('/signin',signin)
 
 api.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next();
 });
 api.use((req, res, next) => {
@@ -37,8 +33,8 @@ api.use((req, res, next) => {
     if(typeof auth!=='undefined' && auth==tokenApi) {
         next()
     } else {
-        res.json({error:'Permission denied!'});
+        res.json({error:'Permission denied!'})
     }
 })
 
-exports.api = functions.https.onRequest(api);
+exports.api = functions.https.onRequest(api)
